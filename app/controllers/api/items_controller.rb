debf1091f87e881aa462686be9285c0ff35ff402
Haroon Azhar <blazintee15@gmail.com>
2019-05-16 05:21:27 +0500

    items controler set

---
+++ app/controllers/api/items_controller.rb
@@ -0,0 +1,35 @@
+class Api::ItemsController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Item.all
+  end
+
+  def show
+    respond_with Item.find(params[:id])
+  end
+
+  def create
+    respond_with :api, Item.create(item_params)
+  end
+
+  def destroy
+    respond_with Item.destroy(params[:id])
+  end
+
+  def update
+    item = Item.find(params['id'])
+    item.update(item_params)
+    respond_with Item, json: item
+  end
+
+  private
+
+  def item_params
+    params.require(:event).permit(
+      :id,
+      :name,
+      :quantity
+    )
+  end
+end
