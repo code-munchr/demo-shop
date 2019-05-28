31a55469b97782991a0d8150252d9f42c6af8e04
Haroon Azhar <blazintee15@gmail.com>
2019-05-28 23:03:49 +0500

    teamplate set

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( style.css )
 Rails.application.config.assets.precompile += %w( menu.css )
 
+
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
